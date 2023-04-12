3ddf2f4619d6a32125b671f702d6d31d111acd62
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-04-12 16:31:04 +0300

    change MainActivity java class

--- app/src/main/java/com/example/android_laboratorna3/MainActivity.java
+++ app/src/main/java/com/example/android_laboratorna3/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.android_laboratorna3;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String name ="undefined";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void saveName(View view) {
+
+        // получаем введенное имя
+        EditText nameBox = findViewById(R.id.nameBox);
+        name = nameBox.getText().toString();
+    }
+    public void getName(View view) {
+
+        // получаем сохраненное имя
+        TextView nameView = findViewById(R.id.nameView);
+        nameView.setText(name);
     }
 }
